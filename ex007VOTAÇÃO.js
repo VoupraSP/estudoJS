3dcdb6a37306fcc5b958a8bea6a7e2cef7235f35
rayamfonseca_ <rayam.io.fonseca@gmail.com>
2023-10-09 15:27:10 -0300

    adding exe

---
+++ ex007VOTAÇÃO.js
@@ -0,0 +1,9 @@
+var idade = 16; 
+
+if (idade < 16 ) {
+    console.log('NÃO VOTA')
+} else if (idade < 18 || idade >= 67) { 
+        console.log('VOTO OPCIONAL')
+} else {
+    console.log('VOTO OBRIGATÓRIO')
+}
