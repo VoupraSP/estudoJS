b26988fbbc71d35bd1410ebf8423fce111c967b8
rayamfonseca_ <rayam.io.fonseca@gmail.com>
2023-10-19 15:47:19 -0300

    add strings

---
+++ Strings/exSliceReplace.js
@@ -0,0 +1,5 @@
+const name = 'Rayam Fonsêca'
+console.log(name.slice(0, 5)) // SLICE (mostra uma parte desejada)
+console.log(name.split(' ')[0]) //SPLIT retorna um array por separador
+console.log(name.split(' ')[1])
+console.log(name.split('a'))
