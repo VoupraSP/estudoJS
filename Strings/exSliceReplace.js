fc71ce2b180fee101f63bde22acfde3cba494d7d
rayamfonseca_ <rayam.io.fonseca@gmail.com>
2023-10-20 10:32:40 -0300

    popPush

--- Strings/exSliceReplace.js
+++ Strings/exSliceReplace.js
@@ -1,5 +1,9 @@
 const name = 'Rayam Fonsêca'
 console.log(name.slice(0, 5)) // SLICE (mostra uma parte desejada)
+const itens = ['Feijão', 'Arroz', 'Macarrão', 'batata', 'alface']
+const copy = itens.slice(0, 3)
+console.log(copy)
+
 console.log(name.split(' ')[0]) //SPLIT retorna um array por separador
 console.log(name.split(' ')[1])
 console.log(name.split('a'))
---
+++ Strings/popPushUnsh.js
@@ -0,0 +1,14 @@
+let names = []
+names.push('Rayam') //Coloca ao final do array
+names.push('Náthaly')
+names.push('Morgana')
+console.log(names, names.length)
+
+names.unshift('lux')//Coloca ao inicio do array
+console.log(names, names.length)
+
+names.pop() //Remove o último
+console.log(names, names.length)
+
+names.shift() //Remove o primeiro elemento
+console.log(names, names.length)
