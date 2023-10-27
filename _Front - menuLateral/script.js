84bd9cd740125130644552f3d19fb494cb4fc4b3
Rayam Fonsêca <133929446+VoupraSP@users.noreply.github.com>
2023-10-27 10:03:20 -0300

    add function handleMenu

---
+++ _Front - menuLateral/script.js
@@ -0,0 +1,18 @@
+feather.replace();
+
+const menuEl = document.getElementById('menu');
+const lateralMenuEl = document.getElementById('lateral-menu');
+
+menuEl.addEventListener('click', handleMenu);
+function handleMenu() {
+    const lateralMenuClassList = Array.from(lateralMenuEl.classList);
+    const isActive = lateralMenuClassList.find(el => el === 'active');
+
+    if(isActive) {
+        lateralMenuEl.classList.remove('active')
+    } else {
+        lateralMenuEl.classList.add('active')
+    }
+
+}
+
